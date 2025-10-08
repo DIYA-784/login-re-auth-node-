6676644618e1cf3550c6267c7d72c6de540dc452
Diya Dutta <125148962+DIYA-784@users.noreply.github.com>
2025-10-08 17:54:56 +0530

    Add files via upload

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const Router = express.Router();
 const authController = require('../controllers/authController');
+const { identifier } = require('../middlewares/identification');
 
 Router.post('/signup', authController.signup);
+
+Router.post('/signin', authController.signin);
+
+Router.post('/signout', identifier, authController.signout);
+
+Router.patch('/send-verification-code', identifier, authController.sendVerificationCode);
+
+Router.patch('/verify-verification-code', identifier, authController.verifyVerificationCode);
+
+Router.patch('/change-password', identifier, authController.changePassword);
 
 module.exports = Router;
